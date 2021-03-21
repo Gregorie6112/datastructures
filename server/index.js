c769f1bf280e9edf43cbee7e906aa8a94f02dc33
Gregorie Calderon <calderon.greg6112@gmail.com>
2021-03-21 00:32:20 -0700

    Added webpack and a package json for installing dependencies

---
+++ server/index.js
@@ -0,0 +1,14 @@
+const path = require('path');
+const express = require('express');
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+app.use(express.static(path.join(__dirname, '../index.html')));
+
+app.get('/', (req, res) => {
+  res.status(200).send('Hello from get');
+})
+
+app.listen(PORT, () => {
+  console.log('Server is running on 3000');
+})
