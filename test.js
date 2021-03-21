2042e27414eb05309e875545e52cb443445ca5e2
Gregorie Calderon <calderon.greg6112@gmail.com>
2021-03-21 00:57:05 -0700

    Added 4 test cases basic for a BST will add some more in depth

--- test.js
+++ test.js
@@ -1,3 +1,12 @@
-test('Should traverse all of the Binary Search Tree', () => {
-  expect([]).toBe([]);
+test('Should Create an Empty Tree', () => {
+  expect().toBe();
+});
+test('Should Insert A Value Into the Binary Search Tree', () => {
+  expect().toBe();
+});
+test('Should Find A Value In the Binary Search Tree', () => {
+  expect().toBe();
+});
+test('Should Delete A Value In the Binary Search Tree', () => {
+  expect().toBe();
 });
