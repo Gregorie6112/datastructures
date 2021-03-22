5e7dcb30f02c9b53caff14987b22dad12ec8f5e9
Gregorie Calderon <calderon.greg6112@gmail.com>
2021-03-22 00:38:15 -0700

    Implemented adding a node test and working, used depthFirstSearch

--- test.js
+++ test.js
@@ -4,17 +4,34 @@ test('Should Create an Empty Tree', () => {
   expect(BinarySearchTree).toBe(BinarySearchTree);
 });
 test('Should Insert A Value Into the Binary Search Tree', () => {
-  expect('').toBe();
+  let tree = new BinarySearchTree(10);
+  tree.insert(9);
+  let values = [];
+  let depthFirstHelper = (node) => {
+    if (node === null) {
+      return;
+    }
+    if (node.value !== undefined) {
+      values.push(node.value);
+    }
+    depthFirstHelper(node.left);
+    depthFirstHelper(node.right);
+  }
+  let depthFirstSearch = (tree) => {
+    return depthFirstHelper(tree);
+  }
+  depthFirstSearch(tree);
+  expect(values).toContain(9);
 });
-test('Should Find A Value In the Binary Search Tree', () => {
-  expect('').toBe();
-});
-test('Should Delete A Value In the Binary Search Tree', () => {
-  expect('').toBe();
-});
-test('Should Map All Values In the Binary Search Tree and return a sorted array', () => {
-  expect('').toBe();
-});
-test('Should Invert the Binary Search Tree', () => {
-  expect('').toBe();
-});
+// test('Should Find A Value In the Binary Search Tree', () => {
+//   expect('').toBe();
+// });
+// test('Should Delete A Value In the Binary Search Tree', () => {
+//   expect('').toBe();
+// });
+// test('Should Map All Values In the Binary Search Tree and return a sorted array', () => {
+//   expect('').toBe();
+// });
+// test('Should Invert the Binary Search Tree', () => {
+//   expect('').toBe();
+// });
