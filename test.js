12d59c4d6eadb7a45d67fce7075512a46c150270
Gregorie Calderon <calderon.greg6112@gmail.com>
2021-04-12 11:20:43 -0700

    Pushing up latest changes

--- test.js
+++ test.js
@@ -1,67 +1,53 @@
+const react  = require('react');
+const renderer  = require('react-test-renderer');
 const BinarySearchTree = require('./src/testFiles/BinarySearchTree.js');
 const BubbleSort = require('./src/testFiles/BubbleSort.js');
 
-test('Bubble Sort Should Exist', () => {
-  expect(BubbleSort).not.toBe(undefined);
-});
-test('Bubble Sort should sort an array', () => {
-  let unSorted = [10,3,5,9,0,1,4,6,12,14];
-  let internalSort = unSorted.sort((a,b) => a-b);
-  let bubbleSort = BubbleSort(unSorted);
-  for (var i = 0; i < bubbleSort.length; i++) {
-    expect(bubbleSort[i]).toBe(internalSort[i]);
-  };
-});
+describe('React Component', () => {
 
+})
+describe('Bubble Sort', () => {
 
+  it('Bubble Sort Should Exist', () => {
+    expect(BubbleSort).not.toBe(undefined);
+  });
 
+  it('Bubble Sort should sort an array', () => {
+    let unSorted = [10,3,5,9,0,1,4,6,12,14];
+    let internalSort = unSorted.sort((a,b) => a-b);
+    let bubbleSort = BubbleSort(unSorted);
+    for (var i = 0; i < bubbleSort.length; i++) {
+      expect(bubbleSort[i]).toBe(internalSort[i]);
+    };
+  });
+})
+describe('Binary Tree', () => {
 
+  it('Should Create an Empty Tree', () => {
+    expect(BinarySearchTree).toBe(BinarySearchTree);
+  });
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-test('Should Create an Empty Tree', () => {
-  expect(BinarySearchTree).toBe(BinarySearchTree);
-});
-test('Should Insert A Value Into the Binary Search Tree', () => {
-  let tree = new BinarySearchTree(10);
-  tree.insert(9);
-  let values = [];
-  let depthFirstHelper = (node) => {
-    if (node === null) {
-      return;
+  it('Should Insert A Value Into the Binary Search Tree', () => {
+    let tree = new BinarySearchTree(10);
+    tree.insert(9);
+    let values = [];
+    let depthFirstHelper = (node) => {
+      if (node === null) {
+        return;
+      }
+      if (node.value !== undefined) {
+        values.push(node.value);
+      }
+      depthFirstHelper(node.left);
+      depthFirstHelper(node.right);
     }
-    if (node.value !== undefined) {
-      values.push(node.value);
+    let depthFirstSearch = (tree) => {
+      return depthFirstHelper(tree);
     }
-    depthFirstHelper(node.left);
-    depthFirstHelper(node.right);
-  }
-  let depthFirstSearch = (tree) => {
-    return depthFirstHelper(tree);
-  }
-  depthFirstSearch(tree);
-  expect(values).toContain(9);
-});
+    depthFirstSearch(tree);
+    expect(values).toContain(9);
+  });
+})
 // test('Should Find A Value In the Binary Search Tree', () => {
 //   expect('').toBe();
 // });
